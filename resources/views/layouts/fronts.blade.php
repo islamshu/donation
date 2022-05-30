a8b8007d3c8f7598141a84ef83897f9883929ac2
islamshu12 <islamshu12@gmail.com>
2022-05-30 16:58:19 +0300

    d

--- resources/views/layouts/fronts.blade.php
+++ resources/views/layouts/fronts.blade.php
@@ -111,7 +111,7 @@
     color: #fff;
     }
     #wrapperContent2 {
-    background: #50aaaa  !important;
+    background: #556beb  !important;
     border: 1px solid #e0dfd5 !important;
     padding: 11px 11px 0 !important;
     /* color: red !important; */
